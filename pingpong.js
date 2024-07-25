b336d67f483883283674df0b99670eefcc106696
gu <168776351+croaian@users.noreply.github.com>
2024-07-25 17:27:08 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,29 +1,25 @@
-// aqui eu vou criar a bolinha
-let xBolinha=300;
-let yBolinha=200;
-let tamBolinha=55;
-let xvelBolinha=6
-let yvelBolinha=6
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
 
 function setup(){
     //aqui vou criar minha "mesa"
-    createCanvas(600,400);
+    createCanvas(1200,800);
 }
 
 function draw(){
-    //aqui vou por a cor da minha "mesa"
-    //a cor da mesa é em rgb
-    background(105,0,231);
-    criaBolinha(xBolinha,yBolinha,tamBolinha)
-    moveBolinha()
-}
+    background(248,128,39); 
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    moveBolinha();
 
-function criaBolinha(xBolinha,yBolinha,tamBolinha){
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
     circle (xBolinha,yBolinha,tamBolinha);
 }
 
-//função move bolinha
 function moveBolinha(){
-    xBolinha=xvelBolinha+xBolinha
-    yBolinha=yvelBolinha+yBolinha
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
 }
